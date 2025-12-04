29454c5c2f952da72ce78e7204336f5e23435292
janreynoldsp4 <TrauxBrew7932@outlook.com>
2025-12-05 01:02:31 +0800

    add secure.java, index.ts, mock-analytics.js

---
+++ frontend/web/src/adapter-user/secure.java
@@ -0,0 +1,2 @@
+// Auto-generated file: D:\MyCode\zama\demo\frontend\web\src\adapter-user\secure.java
+// Generated at: 2025-10-17T07:37:14.792Z
---
+++ notification-layouts-queries.go
@@ -0,0 +1,2 @@
+// Auto-generated file: D:\MyCode\zama\demo\notification-layouts-queries.go
+// Generated at: 2025-10-17T07:37:14.765Z
